7192680386ac4d2cda3749d049516cc57acb660f
Fukubi <deividgabrielproc@gmail.com>
2020-07-27 21:37:21 -0300

    CRUD completo

--- back-end/src/controllers/PessoaController.js
+++ back-end/src/controllers/PessoaController.js
@@ -1,6 +1,8 @@
 const crypto = require('crypto');
 
 const connection = require('../database/connection');
+const { update } = require('../database/connection');
+const { response } = require('express');
 
 module.exports = {
     async index(request, response) {
@@ -28,6 +30,30 @@ module.exports = {
         const { id } = request.params;
 
         await connection('pessoas').where('id', id).delete();
+
+        return response.status(204).send();
+    },
+
+    async updateName(request, response) {
+        const { id, nameAlt } = request.body;
+        
+        await connection('pessoas').where('id', id).update({ name: nameAlt });
+
+        return response.status(204).send();
+    },
+
+    async updateTelefone(request, response) {
+        const { id, telefoneAlt } = request.body;
+
+        await connection('pessoas').where('id', id).update({ telefone: telefoneAlt })
+
+        return response.status(204). send();
+    },
+
+    async updateEmail(request, response) {
+        const { id, emailAlt } = request.body;
+
+        await connection('pessoas').where('id', id). update({ email: emailAlt })
 
         return response.status(204).send();
     }
--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -5,7 +5,15 @@ const PessoaController = require('./controllers/PessoaController');
 const routes = express.Router();
 
 routes.get('/pessoas', PessoaController.index);
+
 routes.post('/pessoas', PessoaController.create);
+
 routes.delete('/pessoas/:id', PessoaController.delete);
+
+routes.put('/pessoas/nome', PessoaController.updateName);
+
+routes.put('/pessoas/telefone', PessoaController.updateTelefone);
+
+routes.put('/pessoas/email', PessoaController.updateEmail);
 
 module.exports = routes;
